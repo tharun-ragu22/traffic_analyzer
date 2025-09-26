b9becf1e6dd3631a31708fb512ba87f530882773
Tharun Ragulan <t2ragula@uwaterloo.ca>
2025-09-26 14:08:48 -0400

    can add map with traffic to webpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import maplibregl from "maplibre-gl";
+import "./App.css";
 
 interface Message {
   text: string;
 }
 
 function App() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!mapContainer.current) return;
+
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json", // Map style to use
+      center: [-79.34711, 43.766599], // Initial map center in [lng, lat]
+      zoom: 13, // Initial map zoom level
+
+    });
+    return () => map.remove();
+  });
 
   const handleButtonClick = async () => {
-    const endpoint = 'http://localhost:8080/log';
-    const message: Message = { text: 'Hello from React!' };
+    const endpoint = "http://localhost:8080/log";
+    const message: Message = { text: "Hello from React!" };
 
     try {
       const response = await fetch(endpoint, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(message),
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Response from server:', data);
+        console.log("Response from server:", data);
       }
     } catch (error) {
-      console.error('Error sending message:', error);
+      console.error("Error sending message:", error);
     }
-  }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -44,9 +59,14 @@ function App() {
         >
           Learn React please
         </a>
-        <button id="test-button" onClick={handleButtonClick}>Test Button</button>
+        <button id="test-button" onClick={handleButtonClick}>
+          Test Button
+        </button>
       </header>
-      
+      <div
+        ref={mapContainer}
+        style={{ width: 600, height: 400, border: "1px solid red" }}
+      ></div>
     </div>
   );
 }
